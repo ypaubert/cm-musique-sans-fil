a0810b20a37af0464e6000ac16d557c7e020221e
ypaubert <114052247+ypaubert@users.noreply.github.com>
2022-09-21 09:55:36 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+namespace musiqueSansFil {
+    export function Arreter_la_note_par_radio(canalmidi: number, notemidi: number) {
+        radio.setGroup(1)
+        radio.sendNumber(1000 * canalmidi + notemidi)
+    }
+    export function Jouer_la_note_par_radio(canalmidi: number, notemidi: number) {
+        radio.setGroup(1)
+        radio.sendNumber(1000 * canalmidi + notemidi)
+    }
+}
